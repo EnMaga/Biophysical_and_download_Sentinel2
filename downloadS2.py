d2bd5613db6e3b9713759165a6fe6e7ed2e234a8
EnMaga <en.magazzino@gmail.com>
2025-02-07 12:41:31 +0100

    Uploaded code

--- downloadS2.py
+++ downloadS2.py
@@ -11,22 +11,22 @@ import data_loading as data
 data_path = Path("downloaded_images")
 data_path.mkdir(exist_ok=True)
 
-# Define Area of Interest (AOI)
+# Define Area of Interest (AOI) CHANGE ACCORDINGLY
 aoi_bbox = shapely.box(736415.5330608572, 4971168.900643755, 
                         749662.1820030723, 4982112.861078258).bounds
 
 # Define CRS for the AOI
 aoi_crs = data.CRS(32632)
 
-# Define the time range for the images
+# Define the time range for the images CHANGE ACCORDINGLY
 time_range = (dt.date(2020, 1, 1), dt.date(2024, 12, 30))
 
 # Define the maximum allowed cloud coverage
 max_cloud_coverage = 0.2  # 20%
 
 # SentinelHub API Credentials (Update these)
-copernicushub_config = data.create_configuration(client_id="IL_TUO_ID",  # Replace with your client ID 
-    client_secret="LA_TUA_SECRET")  # Replace with your client secret
+copernicushub_config = data.create_configuration(client_id="YOUR_ID",  # Replace with your client ID 
+    client_secret="YOUR_SECRET")  # Replace with your client secret
 
 # Download images
 images = data.load_satellite_images(
